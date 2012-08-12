a8ceeb7444cb663d5d85e8dab83f736cfd24049e
Gerrit Böselager <g.boeselager@gmail.com>
2012-08-12 21:52:54 +0200

    DataSource

---
+++ Webservice/src/rentACar/DataSource.java
@@ -0,0 +1,83 @@
+package rentACar;
+import java.sql.*;
+
+/**
+ *  
+ * This class contains a few methods for dealing with the MySQL-database.
+ * Every interaction with the database will be handled by static methods of this class.
+ * @author a.steffen
+ *
+ */
+public class DataSource {
+	/**
+	 * Method to get the connection to the database.
+	 * @return Connection to the database.
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection getConnection()
+		throws ClassNotFoundException, SQLException{
+
+			try {
+				//check, if the MySQL JDBC driver is installed. In the other case throw ClassNotFoundException!  
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				//create connection to the database and return a connection-object on success.
+				Connection con;
+				
+				con = DriverManager.getConnection("jdbc:mysql://localhost/rentacar", "root", "");
+		
+				
+				return con;
+				
+			}catch (ClassNotFoundException e) {
+				throw new SQLException("Datenbanktreiber (JAVA JDBC) ist nicht vorhanden.");
+			}catch (SQLException e) {
+				throw new SQLException("Datenbankverbindung fehlgeschlagen.");
+			}	
+	}
+	
+	/**
+	 * Wrapper for executing a simple SQL-query.
+	 * @param query: SQL-query string 
+	 * @return a ResultSet which contains the queried information
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static ResultSet executeQuery(String query) 
+		throws ClassNotFoundException, SQLException{
+		
+		//database connection
+		Connection con = DataSource.getConnection();
+		
+		//create a SQL statement
+		Statement stmt = con.createStatement();
+		
+		//execute query now
+		ResultSet result = stmt.executeQuery(query);
+		
+		return result;
+	}
+
+	/**
+	 * Executes a statement, which might be an INSERT, UPDATE or DELETE statement.
+	 * Call this method if you do not expect a returning ResultSet from the database.
+	 * @param statement: SQL-statement string
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public static void executeNonQuery(String statement) 
+		throws SQLException, ClassNotFoundException{
+		
+		//database connection
+		Connection con = DataSource.getConnection();
+		
+		//create a SQL statement
+		Statement stmt = con.createStatement();
+		
+		//execute the statement now
+		stmt.executeUpdate(statement);
+		
+		//close the database connection 
+		con.close();
+	}
+}
--- Webservice/src/rentACar/RentACar_Webservice.java
+++ Webservice/src/rentACar/RentACar_Webservice.java
@@ -1,5 +1,8 @@
 package rentACar;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class RentACar_Webservice {
 	
 	public RentACar_Webservice() 
@@ -14,7 +17,20 @@ public class RentACar_Webservice {
 	
 	public String sayHello(String name)
 	{
-		return "Hallo " + name;
+		/*DataSource.executeNonQuery("INSERT INTO `group` (id, user_id, name) " +
+				"VALUES (NULL, " + user.getId() + ", '" + name + "')");*/
+		try {
+			ResultSet result = DataSource.executeQuery("SELECT * FROM vehicles");
+			
+			int rowCount = result.last() ? result.getRow() : 0; // Determine number of rows  
+			return "Anzahl " + rowCount;
+		} catch (ClassNotFoundException e) {
+			return "Class not found";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			return e.getMessage();
+		}
+		//return "Hallo2 " + name;
 	}
 	
 	public void meinTest(){
