95c457e6c3bc0efad09b7383abd33e85e84b3d21
Gerrit Böselager <g.boeselager@gmail.com>
2012-08-14 12:58:32 +0200

    Updates

--- Webservice/src/rentACar/RentACar_Webservice.java
+++ Webservice/src/rentACar/RentACar_Webservice.java
@@ -10,11 +10,6 @@ public class RentACar_Webservice {
 		
 	}
 	
-	public int returnInteger()
-	{
-		return 10;
-	}
-	
 	public String sayHello(String name)
 	{
 		/*DataSource.executeNonQuery("INSERT INTO `group` (id, user_id, name) " +
@@ -24,6 +19,7 @@ public class RentACar_Webservice {
 			
 			int rowCount = result.last() ? result.getRow() : 0; // Determine number of rows  
 			return "Anzahl " + rowCount;
+			
 		} catch (ClassNotFoundException e) {
 			return "Class not found";
 		} catch (SQLException e) {
@@ -33,14 +29,11 @@ public class RentACar_Webservice {
 		//return "Hallo2 " + name;
 	}
 	
-	public void meinTest(){
-		//return t;
-	}
-	
-	public Vehicle hey(){
+	public Vehicle getVehicle(){
 		Vehicle v = new Vehicle();
-		v.model = "sasa";
-		v.other = "sdssd";
+		v.setModel("Model Gerrit");
+		v.setOther("Other Gerrit");
+		v.setNumber(777);
 		return v;
 	}
 }
--- Webservice/src/rentACar/Vehicle.java
+++ Webservice/src/rentACar/Vehicle.java
@@ -1,6 +1,33 @@
 package rentACar;
 
 public class Vehicle {
-	public String model;
-	public String other;
+
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	private String model;
+	private String other;
+	private int number;
+	
+	
 }
