215ed80437758dbbffb173421bf1d322498367eb
Alex Steffen <a.steffen@me.com>
2012-08-17 11:56:11 +0200

    customer class added

--- Webservice/src/rentACar/Customer.java
+++ Webservice/src/rentACar/Customer.java
@@ -2,4 +2,13 @@ package rentACar;
 
 public class Customer {
 
+	private int id;
+	private String email;
+	private String forename;
+	private String lastname;
+	private String phone;
+	private String street;
+	private String zip;
+	
+	
 }
