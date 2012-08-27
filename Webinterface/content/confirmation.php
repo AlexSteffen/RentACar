c1d8103e61a8e6c5f77900daad7b0ec16390eb6f
Alex Steffen <a.steffen@me.com>
2012-08-28 00:39:20 +0200

    no real changes

--- Webinterface/content/confirmation.php
+++ Webinterface/content/confirmation.php
@@ -2,6 +2,7 @@
 //load all passed get parameters passed from the site before
 include("parameter.php");
 
+
 if($invalidParameters==true){
     //stop execution of this file
     return;
@@ -15,6 +16,7 @@ $vehicle = $result->return;
 //calculate the difference between the dates to get the total_price
 $rentalDays = Converter::dateDifferenceInDays($startDate, $returnDate);
 $sum = round($vehicle->pricePerDay * $rentalDays, 2);
+
 
 //***>>>> HIER MUSS NOCH PRÜFUNG EINGEBAUT WERDEN, OB DAS AUTO WIRKLICH ZU DEM ZEITPUNKT VERFÜGBAR IST!!!
 
